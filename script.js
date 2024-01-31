fc66b330668602dc889ed2ba900fa56162a82f23
Apu Paul <154053024+apupaulcode@users.noreply.github.com>
2024-01-31 19:51:53 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,25 @@
+const time = document.getElementById('time');
+const timeforrmat = document.getElementById('timeformat');
+
+
+
+document.addEventListener('DOMContentLoaded', ()=>{
+  setInterval(showTime, 1000);
+});
+
+
+const showTime = () => {
+  let date = new Date();
+
+  let hr = date.getHours();
+  let mins = date.getMinutes();
+  let secs = date.getSeconds();
+
+
+  hr = hr<10? `0${hr}`:hr;
+  mins = mins<10? `0${mins}`:mins;
+  secs = secs<10? `0${secs}`:secs;
+
+  time.innerHTML = `${hr} : ${mins} : ${secs}`;
+  timeforrmat.innerHTML = hr>12? "PM" : "AM";
+}
